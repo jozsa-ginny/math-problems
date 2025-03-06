8e9f69d44afe465903ea860aad969706ab22de66
jozsa-ginny <jozsa-ginny@gmx.com>
2025-03-06 16:28:43 +0000

    Add 4.c

---
+++ src/4.c
@@ -0,0 +1,7 @@
+  int main(void) {
+  int x = 5; // change this value to test different scenarios
+  int y = 10; // change this value to test different scenarios
+  int z = x + y;
+  printf("%d", z);
+  return 0;
+}
